05d4604ddba5b256757041f9b1c0f35ee356e4c7
Jaewon Yun <woin2ee@gmail.com>
2022-11-11 23:11:04 +0900

    MenuHandler 에서 메뉴의 잘못된 입력 처리 구현

--- MyCreditManager/CoreObject/MenuHandler.swift
+++ MyCreditManager/CoreObject/MenuHandler.swift
@@ -9,4 +9,50 @@ import Foundation
 
 struct MenuHandler {
     
+    func execute(by menu: Menu?) {
+        guard let menu = menu else { return printError() }
+        
+        switch menu {
+        case .addStudent:
+            addStudent()
+        case .deleteStudent:
+            deleteStudent()
+        case .updateGrade:
+            updateGrade()
+        case .deleteGrade:
+            deleteGrade()
+        case .showTotalGrade:
+            showTotalGrade()
+        case .exit:
+            exit()
+        }
+    }
+    
+    private func addStudent() {
+        
+    }
+    
+    private func deleteStudent() {
+        
+    }
+    
+    private func updateGrade() {
+        
+    }
+    
+    private func deleteGrade() {
+        
+    }
+    
+    private func showTotalGrade() {
+        
+    }
+    
+    private func exit() {
+        
+    }
+    
+    private func printError() {
+        print("뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요.")
+    }
 }
