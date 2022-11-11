b698214c5e217f9562b7217a67ea634ef3b8ea1a
Jaewon Yun <woin2ee@gmail.com>
2022-11-11 23:08:48 +0900

    MenuReceiver 구현

--- MyCreditManager/CoreObject/MenuReceiver.swift
+++ MyCreditManager/CoreObject/MenuReceiver.swift
@@ -9,4 +9,13 @@ import Foundation
 
 struct MenuReceiver {
     
+    func printMenu() {
+        print("원하는 기능을 입력해주세요.")
+        print("1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료")
+    }
+    
+    func inputMenu() -> Menu? {
+        guard let input = readLine() else { return nil }
+        return .init(input: input)
+    }
 }
